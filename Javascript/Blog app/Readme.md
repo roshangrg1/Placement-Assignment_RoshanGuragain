c6c611efa3d9019dbd2f8aa2e7671fa48f4f228c
Roshan Guragain <twistfreaky@gmail.com>
2023-06-06 17:56:42 +0545

    Blog app added

---
+++ Javascript/Blog app/Readme.md
@@ -0,0 +1,33 @@
+
+# Blog app [live link](https://blog-javascript.netlify.app/)
+
+## About 
+    This is a simple project made with the help of pure Html , tailwind Css and javascript.
+
+## Screenshots
+
+![App Screenshot](./screenshot/Screenshot%20(53).png)
+
+
+## What i learned in this project.
+- Revised media query for different screen size adjustment in tailwind.
+- Revised all Css concept like  flex, flexbox, flex direction, justifyContent, alignitem, etc using tailwind css.
+- use case of  Javascript logic and DOM manipulation according to situation.
+
+
+
+## 🚀 About Me
+I'm a full stack Javascript developer..
+
+
+## 🔗 Links
+
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/roshan-guragain-guragain-747aa4245/)
+[![twitter](https://img.shields.io/badge/twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/RoshanGuragain3)
+
+
+##  [My Official portfolio](https://portfolio-roshan.netlify.app/)
+
+
+
+
---
+++ Javascript/Blog app/script.js
@@ -0,0 +1,62 @@
+const container = document.querySelector(".blog-container");
+
+const blogArr = [];
+
+const onPageLoad = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const data = await res.json();
+
+  console.log(data);
+
+  data.map((element, index) => {
+    if (index > 25) return;
+
+    blogArr.push(element);
+    container.insertAdjacentHTML(
+      "afterbegin",
+      `<div class=" bg-[#ffffff] w-[250px] min-h-[250px] flex flex-col justify-between  items-center  p-4 rounded-md drop-shadow-md">
+      <h2 class="text-[blue] font-serif text-xl text-center">${element.title}</h2>
+      <p class="text-center text-[#000]">${element.body}</p>
+    </div>`
+    );
+  });
+};
+onPageLoad();
+
+const titleInput = document.getElementById("title");
+const textInput = document.getElementById("text");
+const userBlogContainer = document.querySelector(".my-blog");
+const hideText = document.querySelector(".hide");
+const deleteButton = document.querySelector(".btn");
+
+const addData = async (event) => {
+  event.preventDefault();
+
+  if (!titleInput.value && !textInput.value) {
+    alert("Please Fill the data properly");
+  }
+
+  hideText.classList.add("hidden");
+
+  deleteButton.classList.remove("hidden");
+
+  const title = titleInput.value;
+  const text = textInput.value;
+
+  userBlogContainer.insertAdjacentHTML(
+    "afterbegin",
+    `
+  <div class="w-[250px] min-h-[250px] flex flex-col justify-between items-center bg-[#ffffff] p-4 rounded-md drop-shadow-md">
+      <h2 class="text-[blue] font-serif text-xl text-center">${title}</h2>
+      <p class="text-center text-[#14213d]">${text}</p>
+    </div>
+  `
+  );
+
+  titleInput.value = "";
+  textInput.value = "";
+};
+
+deleteButton.addEventListener("click", () => {
+  location.reload();
+});
